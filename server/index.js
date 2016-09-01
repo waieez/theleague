c2e68236602b0bc352865115c7dba186f6446952
eli xian <elixian@gmail.com>
2016-09-01 10:59:11 -0700

    Sketch out some functionality

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 var app = require("./app")
 var PORT = process.env.PORT || 3000
 app.listen(PORT, function () {
-  console.log('listening on port: ', port);
+  console.log('listening on port: ', PORT);
 });
--- test/test.js
+++ test/test.js
@@ -4,6 +4,8 @@ var request = require('supertest');
 
 var app = require('../server/app');
 
+// TODO: some setup
+
 describe("Login", function (done) {
 
   it("Should handle '/login'", function (done) {
@@ -20,7 +22,7 @@ describe("Login", function (done) {
   })
 })
 
-describe("Signup", function (done) {
+describe("Signup", function () {
 
   it("Should handle '/signup'", function (done) {
     request(app)
@@ -33,5 +35,39 @@ describe("Signup", function (done) {
         }
         done()
       })
+  })
+})
+
+xdescribe("Auth", function () {
+  xit("Should reject users with invalid sessions", function (done) {
+
+  })
+  xit("Should create a session for a auth user", function (done) {
+
+  })
+
+})
+
+xdescribe("Profile", function () {
+  xit("Auth users should be able to edit their profiles", function (done) {
+
+  })
+})
+
+xdescribe("Preferences", function () {
+  xit("Auth users should be able to edit their preferences", function (done) {
+
+  })
+})
+
+xdescribe("Matches", function () {
+  xit("Auth users should be able to get a set of matches based on their preferences", function (done) {
+
+  })
+})
+
+xdescribe("Votes", function () {
+  xit("Auth users should be bale to like match", function (done) {
+
   })
 })
